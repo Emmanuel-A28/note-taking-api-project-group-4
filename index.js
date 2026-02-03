fe8304498b926b610bb601314f8374d978651b79
Salau Aisha Omowunmi <salauaisha53@gmail.com>
2026-02-03 19:07:23 +0100

    Set up basic server and dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+    res.send('server is running');
+});
+app.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`);
+});
