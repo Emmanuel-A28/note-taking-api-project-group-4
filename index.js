89ce2e92b485bc7a23eb47bfa75b23e107d48dfd
Henry Christopher <chrisraphael211@gmail.com>
2026-02-04 10:57:57 +0100

    Added Update, Patch and Delete Routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.use(express.json());
 
 //creating an array to store notes
 let notes = [];
+console.log(notes)
 let currentId = 1;
 app.post('/notes', (req, res) => {
     const { title, content } = req.body;
@@ -33,6 +34,31 @@ app.get('/notes/:id', (req, res) => {
         return res.status(404).json({ error: 'Note not found.' });
     }
     res.status(200).json(note);
+});
+//update existing note
+app.patch('/notes/:id', (req, res) => {
+    const noteId = parseInt(req.params.id);
+    const updates = req.body;
+    const note = note.find(n => n.id === noteId);
+    if (!note) {
+        return res.status(404).json({error: "Note not found" });
+    }
+    Object.assign(note, updates);
+    res.json({message: "Note patched successfully!", note
+    });
+});
+//delete existing note
+app.get('/notes/:id', (req, res) => {
+    const noteId = parseInt(req.params.id);
+    const noteExists = notes.some(n => n.id === noteId);
+    if (!noteExists) {
+        return res.status(404).json({ error: "Note not found. Nothing to delete." });
+    }
+    notes = notes.filter(n => n.id !== noteId);
+    res.status(200).json({
+        message: `Note ${noteId} has been deleted.`,
+        remainingNotes: notes
+    });
 });
 //starting the server
 const port = 3000;
